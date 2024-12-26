d7eef07d84b0d05ab7d3856a83022d765f4e0e15
Tangimark <mark@teamtangible.com>
2024-12-26 08:26:00 -0500

    add settings page

--- includes/admin/index.php
+++ includes/admin/index.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once __DIR__.'/settings.php';
+require_once __DIR__.'/settings-tabs.php';
--- includes/templates/admin-templates/template.php
+++ includes/templates/admin-templates/template.php
@@ -1,4 +1,16 @@
-<?php
+<?php?>
 
-echo '<h1>Hi Mark!</h1>';
+<select name="sorting-type" class="sorting-type-selector">
+  <option value="Bubble Sort">Bubble Sort</option>
+  <option value="Selection Sort">Selection Sort</option>
+  <option value="Insertion Sort">Insertion Sort</option>
+</select>
+
+<input type="radio" id="custom-data" name="custom_or_random_data" value="CustomData">
+<input type="radio" id="random-data" name="custom_or_random_data" value="RandomData">
+
+
+<button id='start-sorting-btn'>Start Sorting!</button>
+
+<?php
 
